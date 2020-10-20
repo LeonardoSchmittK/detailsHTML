22d1f12e216145a917c702dc7903aeb6d4a2d7cc
Leonardo Schmitt <Leoeyeschmittk@gmail.com>
2020-10-19 22:33:37 -0300

    main goal of the project finished. Adding readme and screenshot

---
+++ index.js
@@ -0,0 +1,35 @@
+const container = window.document.querySelector(".container");
+const planets = [
+  "Mercury",
+  "Venus",
+  "Earth",
+  "Mars",
+  "Jupiter",
+  "Saturn",
+  "Uranus",
+  "Neptune",
+];
+const txt = `&nbsp&nbsp&nbsp Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia quas maxime vel aliquam non itaque beatae ab nesciunt ad repudiandae.`;
+
+function addDetails() {
+  let i = 0;
+  while (i < planets.length) {
+    let details = window.document.createElement("details");
+    let summary = window.document.createElement("summary");
+    let description = window.document.createElement("p");
+    description.innerHTML = txt;
+    summary.innerHTML = planets[i];
+    details.className = "planetConfig ";
+    summary.className = "planetTitle";
+    description.className = "planetDescription";
+    details.appendChild(description);
+    details.appendChild(summary);
+    container.appendChild(details);
+    ++i;
+  }
+}
+
+addDetails();
+
+
+console.clear()
---
+++ readme.md
@@ -0,0 +1,1 @@
+# Detail tag in HTML 
